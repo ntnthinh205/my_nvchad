03c56b15bfa99c902a193e982c5e478864ad7a8a
Thịnh Nguyễn <ntnthinh205@gmail.com>
2026-05-09 15:38:33 +0700

    edit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,7 +6,7 @@ local map = vim.keymap.set
 local os_name = vim.loop.os_uname().sysname
 local is_mac = os_name == "Darwin"
 local is_linux = os_name == "Linux"
-local is_windows = os_name:match("Windows") ~= nil
+local is_windows = os_name:match "Windows" ~= nil
 
 -- ========================
 -- CORE MAPPINGS (ALL OS)
@@ -15,7 +15,6 @@ local is_windows = os_name:match("Windows") ~= nil
 map("n", ";", ":", { desc = "CMD mode" })
 
 map("i", "jk", "<Esc>", { noremap = true, silent = true })
-
 map({ "n", "i", "v" }, "<C-s>", "<cmd>w<CR>")
 
 map("n", "<leader>cc", ":CopyDiagnostic<CR>", { desc = "Copy diagnostic" })
@@ -24,8 +23,8 @@ map("n", "<Esc>", ":nohlsearch<CR>")
 map("n", "q", ":nohlsearch<CR>")
 map("v", "q", "<Esc>")
 
-map("n", "J", "5jzz", { silent = true })
-map("n", "K", "5kzz", { silent = true })
+map("n", "J", "5j", { silent = true })
+map("n", "K", "5k", { silent = true })
 
 map("n", "<C-h>", "<C-w>h", { silent = true })
 map("n", "<C-j>", "<C-w>j", { silent = true })
@@ -45,26 +44,31 @@ map("n", "<leader>=", "<C-w>=", { desc = "equalize splits" })
 map("n", "Q", ":wa<CR>:qa<CR>", { silent = true })
 
 map("n", "<leader>tf", function()
-    require("nvchad.term").toggle { pos = "float", id = "floatTerm", float_opts={
-        row = 0.02,
-        col = 0.05,
-        width = 0.9,
-        height = 0.9
-    }}
+    require("nvchad.term").toggle {
+        pos = "float",
+        id = "floatTerm",
+        float_opts = {
+            row = 0.02,
+            col = 0.05,
+            width = 0.9,
+            height = 0.9,
+        },
+    }
 end, { desc = "terminal toggle floating term" })
 
 if is_mac then
-  map("n", "<D-a>", "ggVG", { silent = true })
-  map("n", "<D-s>", ":wa<CR>", { silent = true })
-  map("n", "<A-h>", ":vertical resize -2<CR>", { silent = true })
-  map("n", "<A-l>", ":vertical resize +2<CR>", { silent = true })
-  map("n", "<A-j>", ":resize -2<CR>", { silent = true })
-  map("n", "<A-k>", ":resize +2<CR>", { silent = true })
+    map("n", "<D-a>", "ggVG", { silent = true })
+    map("n", "<D-s>", ":wa<CR>", { silent = true })
+    map("n", "<A-h>", ":vertical resize -2<CR>", { silent = true })
+    map("n", "<A-l>", ":vertical resize +2<CR>", { silent = true })
+    map("n", "<A-j>", ":resize -2<CR>", { silent = true })
+    map("n", "<A-k>", ":resize +2<CR>", { silent = true })
 else
-  map("n", "<C-a>", "ggVG", { silent = true })
-  map("n", "<C-s>", ":wa<CR>", { silent = true })
-  map("n", "<M-h>", ":vertical resize -2<CR>", { silent = true })
-  map("n", "<M-l>", ":vertical resize +2<CR>", { silent = true })
-  map("n", "<M-j>", ":resize -2<CR>", { silent = true })
-  map("n", "<M-k>", ":resize +2<CR>", { silent = true })
+    map("n", "<C-a>", "ggVG", { silent = true })
+    map("n", "<C-s>", ":wa<CR>", { silent = true })
+    map("n", "<M-h>", ":vertical resize -2<CR>", { silent = true })
+    map("n", "<M-l>", ":vertical resize +2<CR>", { silent = true })
+    map("n", "<M-j>", ":resize -2<CR>", { silent = true })
+    map("n", "<M-k>", ":resize +2<CR>", { silent = true })
 end
+
--- lua/options.lua
+++ lua/options.lua
@@ -15,3 +15,14 @@ vim.api.nvim_set_hl(0, "WinSeparator", {
 -- vim.api.nvim_set_hl(0, "NormalNC", {
 --   bg = "#1b1f27",
 -- })
+
+-- init.lua
+
+local opt = vim.opt
+
+opt.tabstop = 4      -- Số khoảng trắng hiển thị cho một phím Tab vật lý
+opt.shiftwidth = 4   -- Số khoảng trắng được dùng khi thụt lề (với lệnh >> hoặc <<)
+opt.expandtab = true -- Chuyển đổi Tab vật lý thành khoảng trắng (Spaces)
+opt.softtabstop = 4  -- Số khoảng trắng mà phím Tab/Backspace sẽ xử lý khi gõ
+opt.smartindent = true -- Tự động thụt lề thông minh khi xuống dòng
+
